092ae4d1cc42ebeb847f9b3f4327da11187f66ae
Craig Thacker <craigthackerx@gmail.com>
2022-03-28 23:26:56 +0100

    update test

--- terraform/test.tf
+++ terraform/test.tf
@@ -1,12 +1,17 @@
 resource "azurerm_resource_group" "example_rg" {
+  name     = "rg-${var.short}-${var.loc}-${terraform.workspace}-web-01"
   location = local.location
-  name     = "rg-${var.short}-${var.loc}-${terraform.workspace}-exm-01"
   tags     = local.tags
 }
 
 resource "azurerm_application_security_group" "example_asg" {
+  count               = 4
+  name                = "asg-${var.short}-${var.loc}-${terraform.workspace}-web-${format("%02d", count.index + 1)}"
   location            = local.location
-  name                = "asg-${var.short}-${var.loc}-${terraform.workspace}-exm-01"
   resource_group_name = azurerm_resource_group.example_rg.name
   tags                = local.tags
+}
+
+output "asg_output" {
+  value = azurerm_resource_group.example_rg.*.name
 }
